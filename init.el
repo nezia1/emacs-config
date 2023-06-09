b46aa74081b5bcb2e257e5b6d0a99d1460108600
Anthony Rodriguez <anthony@nezia.dev>
2023-06-09 10:06:53 +0200

    fix org mode version mismatch bug - https://debbugs.gnu.org/cgi/bugreport.cgi?bug=62762

--- init.el
+++ init.el
@@ -1,1 +1,31 @@
+; initialize straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+	       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+            (bootstrap-version 6))
+    (unless (file-exists-p bootstrap-file)
+          (with-current-buffer
+	            (url-retrieve-synchronously
+		               "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+			                'silent 'inhibit-cookies)
+		          (goto-char (point-max))
+			        (eval-print-last-sexp)))
+      (load bootstrap-file nil 'nomessage))
+
+; install use-package 
+(straight-use-package 'use-package)
+
+; integrate straight.el with use-package
+(use-package straight
+	       :custom (straight-use-package-by-default t))
+
+; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=62762
+(use-package org)
+
+; follow symlinks in order for version control to work
+(setq vc-follow-symlinks t)
+
+; load literate config
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "README.org")))
+
+
