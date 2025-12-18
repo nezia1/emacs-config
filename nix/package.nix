f668605a12a5770bc44a770023bb57d7c8ac5637
Anthony Rodriguez <anthony@nezia.dev>
2025-12-18 11:03:08 +0100

    fix: include info pages from emacsDeps; regenerate DIR

--- nix/package.nix
+++ nix/package.nix
@@ -21,43 +21,59 @@
   pandoc,
   _0xproto,
   nerd-fonts,
+  glibcInfo,
+  texinfo,
 }: let
-  emacsWithPackages = (emacsPackagesFor emacs).emacsWithPackages (
-    epkgs:
-      with epkgs; [
-        catppuccin-theme
-        corfu
-        envrc
-        exec-path-from-shell
-        ultra-scroll
-        org-appear
-        org-modern
-        ox-pandoc
-        magit
-        eldoc-box
-        kind-icon
-        cape
-        eglot-booster
-        marginalia
-        vertico
-        orderless
-        eat
-        engrave-faces
-        php-mode
+  emacsWithPackages =
+    (emacsPackagesFor emacs).emacsWithPackages
+    (
+      epkgs:
+        with epkgs; [
+          catppuccin-theme
+          corfu
+          envrc
+          exec-path-from-shell
+          ultra-scroll
+          org-appear
+          org-modern
+          ox-pandoc
+          magit
+          eldoc-box
+          kind-icon
+          cape
+          eglot-booster
+          marginalia
+          vertico
+          orderless
+          eat
+          engrave-faces
+          php-mode
 
-        # modes
-        nix-ts-mode
-        (epkgs.treesit-grammars.with-grammars (grammars:
-          with grammars; [
-            tree-sitter-nix
-            tree-sitter-c
-            tree-sitter-cpp
-            tree-sitter-python
-            tree-sitter-java
-            tree-sitter-yaml
-          ]))
-      ]
-  );
+            # modes
+          nix-ts-mode
+          (epkgs.treesit-grammars.with-grammars (grammars:
+            with grammars; [
+              tree-sitter-nix
+              tree-sitter-c
+              tree-sitter-cpp
+              tree-sitter-python
+              tree-sitter-java
+              tree-sitter-yaml
+            ]))
+        ]
+    );
+
+  # This is here so that instead of having symlinked info pages from the emacs package, we have a derivation with the files directly copied.
+  # This is necessary because we will be using `symlinkJoin` to put all the info pages together
+  emacsWithPackages' = symlinkJoin {
+    inherit (emacsWithPackages) name;
+    paths = [emacsWithPackages];
+    postBuild = ''
+      original_info=$(realpath $out/share/info)
+      rm $out/share/info
+      cp -rs "$original_info" $out/share/info
+    '';
+  };
 
   # https://wiki.nixos.org/wiki/TexLive#Combine_Sets
   tex = texliveMedium.withPackages (
@@ -106,16 +122,24 @@
 
       tex
       pandoc
+      glibcInfo
     ];
+    extraOutputsToInstall = ["man" "doc" "info"];
   };
 
   emacsWrapped = symlinkJoin {
     name = "emacs-wrapped";
-    paths = [emacsWithPackages];
-    nativeBuildInputs = [makeWrapper];
+    paths = [emacsWithPackages' emacsDeps];
+
+    nativeBuildInputs = [makeWrapper texinfo];
+
     postBuild = ''
+      shopt -s nullglob
+      for i in $out/share/info/*.info $out/share/info/*.info.gz; do
+              install-info $i $out/share/info/dir
+      done
+
       wrapProgram $out/bin/emacs \
-        --prefix PATH : ${emacsDeps}/bin \
         --prefix INFOPATH : $out/share/info \
         --add-flags --init-directory="${../config}"
     '';
